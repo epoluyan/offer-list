4193e553720b7101da8ab3acd6a084f9e18f8d5b
e.poluyan <e.poluyan@hsdigital.ru>
2022-06-14 20:53:10 +0700

    list-item

---
+++ src/components/Listing.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import Item from './Item';
+
+function Listing({items = []}) {
+  return (
+    <div className="item-list">
+      {items.map((item) => <Item item={item} key={item.listing_id}/>)}
+    </div>
+  );
+}
+
+Listing.propTypes = {
+  items: PropTypes.array.isRequired,
+}
+  
+export default Listing;
